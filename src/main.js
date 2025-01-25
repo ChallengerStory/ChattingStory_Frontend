a3dd95e40cbc7efa5984e40f2ae351fd2f4a1208
1etterh <1etterh.dev@gmail.com>
2025-01-25 16:19:27 +0900

    feat: 기본설정

--- src/main.js
+++ src/main.js
@@ -1,27 +1,28 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import { setupAxiosInterceptors } from '../src/plugins/axios.js';
 import App from './App.vue';
+import installPrimeVue from './plugins/primevue';
 import router from './router';
+import { useAuthStore } from './stores/auth';
+const pinia = createPinia();
 
-import Aura from '@primevue/themes/aura';
-import PrimeVue from 'primevue/config';
-import ConfirmationService from 'primevue/confirmationservice';
-import ToastService from 'primevue/toastservice';
-
+/* styles */
 import '@/assets/styles.scss';
+import '@/assets/syncday/main.css';
 import '@/assets/tailwind.css';
 
 const app = createApp(App);
 
 app.use(router);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {
-            darkModeSelector: '.app-dark'
-        }
-    }
-});
-app.use(ToastService);
-app.use(ConfirmationService);
+app.use(pinia);
+const authStore = useAuthStore();
+await authStore.initializeAuth(); // 초기 인증 상태 설정
+
+setupAxiosInterceptors();
+
+await authStore.initializeAuth(); // 초기 인증 상태 설정
+
+installPrimeVue(app);
 
 app.mount('#app');
--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,11 +15,31 @@ export default defineConfig({
         vue(),
         Components({
             resolvers: [PrimeVueResolver()]
-        })
+        }),
+        vueDevTools()
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace('/^/api/', ''),
+                secure: false, // SSL 관련 검증 비활성화
+                configure: (proxy, options) => {
+                    // proxy 동작 로깅
+                    proxy.on('proxyReq', (proxyReq, req, res) => {
+                        console.log('Proxy Request:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, res) => {
+                        console.log('Proxy Response:', proxyRes.statusCode);
+                    });
+                }
+            }
         }
     }
 });
