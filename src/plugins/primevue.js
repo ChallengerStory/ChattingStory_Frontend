8102c63ed25386eaa32be2f1dd7fc03709d8ab43
1etterh <1etterh.dev@gmail.com>
2025-02-11 14:28:10 +0900

    feat: 다크모드

--- src/plugins/primevue.js
+++ src/plugins/primevue.js
@@ -40,7 +40,7 @@ export default function installPrimeVue(app) {
             preset: SyncDayPreset,
             options: {
                 prefix: 'p',
-                darkModeSelector: 'system'
+                darkModeSelector: '.app-dark'
             }
         }
     });
