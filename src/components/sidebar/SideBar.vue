c0b8078641da70db5ea82b79c735fbc6384151d7
백경석\seok <dno06117@naver.com>
2025-05-20 14:26:31 +0900

    first kyeongseok commit

--- src/components/sidebar/SideBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>Sidebar</div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- src/router/auth.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {
-        path: '/login',
-        name: 'Login',
-        component: () => import('@/views/pages/auth/Login.vue'),
-        meta: { requiresAuth: false }
-    }
-];
